82f300be481ec532614c41ce5d8e0bcab6890781
Dmitry <51994596+dmitry-w3b@users.noreply.github.com>
2023-09-21 21:18:49 +0300

    Добавил пособие по ТСИ

---
+++ tsi/TSI-new/res/x5settings.js
@@ -0,0 +1,94 @@
+(function ( $, x5engine ) {
+	var x = x5engine,
+		s = x.settings,
+		p = s.currentPath,
+		b = x.boot;
+
+	b.push(function () {
+		x.setupDateTime();
+		x.imAccess.showLogout();
+		x.utils.autoHeight();
+		x.cart.ui.updateWidget();
+		x.imGrid.init();
+	});
+	b.push(function () {
+		x.menu({
+			target: '#imMnMn',
+			showCurrent: false,
+			verticalScroll: true,
+			orientation: 'vertical',
+			menuHeight: 48,
+			menuWidth: 205,
+			submenuHeight: 24,
+			submenuWidth: 300,
+			opacity: 0.8,
+			type: 'singleColumn',
+			alignment: 'left',
+			effect: 'fade'
+		});
+	});
+	b.push(function () { x.utils.imPreloadImages([p + 'menu/index_h.png',p + 'menu/page-13_h.png',p + 'menu/page-14_h.png',p + 'menu/------1.-----------------------------------------------------------------------_h.png',p + 'menu/------2.---------------------------------------------------_h.png',p + 'menu/------3.----------------------_h.png',p + 'menu/------4.----------------------------------_h.png',p + 'menu/------5.----------------------------------------------------_h.png',p + 'menu/------6.-----------------------------------------_h.png',p + 'menu/------7.----------------------_h.png',p + 'menu/------8.-------------------------------------------------_h.png',p + 'menu/------9.---------------------------------------------------------_h.png',p + 'menu/------10.---------------------------------------------------------------------------_h.png',p + 'menu/sub.png',p + 'menu/sub_h.png',p + 'menu/sub_f.png',p + 'menu/sub_m.png',p + 'menu/sub_l.png',p + 'menu/sub_f_h.png',p + 'menu/sub_m_h.png',p + 'menu/sub_l_h.png', p + 'res/imLoad.gif', p + 'res/imClose.png']); });
+
+	// ShowBox
+	$.extend(s.imShowBox, {
+		'effect' : 'fade',
+		'shadow' : '',
+		'background' : '#000000',
+		'borderWidth' : {
+			'top': 1,
+			'right': 1,
+			'bottom': 1,
+			'left': 1
+		},
+		buttonRight: {
+			url: p + 'res/b01_r.png',
+			position: {
+				x: -12,
+				y: 0
+			}
+		},
+		buttonLeft: {
+			url: p + 'res/b01_l.png',
+			position: {
+				x: -12,
+				y: 0
+			}
+		},
+		'borderRadius' : '5px 5px 5px 5px',
+		'borderColor' : '#404040 #404040 #404040 #404040',
+		'textColor' : '#000000',
+		'fontFamily' : 'Tahoma',
+		'fontStyle' : 'normal',
+		'fontWeight' : 'normal',
+		'fontSize' : '9pt',
+		'textAlignment' : 'left',
+		'boxColor' : '#FFFFFF',
+		'opacity' : 0.7,
+		'radialBg' : true // Works only in Mozilla Firefox and Google Chrome
+	});
+
+	// PopUp
+	$.extend(s.imPopUp, {
+		'effect' : 'fade',
+		'width' : 500,
+		'shadow' : '',
+		'background' : '#000000',
+		'borderRadius' : 10,
+		'textColor' : '#000000',
+		'boxColor' : '#FFFFFF',
+		'opacity' : 0.7
+	});
+
+	// Tip
+	$.extend(s.imTip, {
+		'borderRadius' : 1,
+		'arrow' : true,
+		'position' : 'right',
+		'effect' : 'none',
+		'showTail' : true
+	});
+
+	// Captcha
+	var codes = s.imCaptcha.offlineCodes;
+	s.loaded = true;
+})( _jq, x5engine );
